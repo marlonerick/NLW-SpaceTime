9fa0ee12f930ebc251857309c72941f40a730e0b
marlonerick <erick.marlon13@hotmail.com>
2023-05-20 21:13:39 -0300

    Aula05 NLW-Spacetime concluido

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -52,11 +52,12 @@ export default function Layout() {
         screenOptions={{
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
+          animation: 'simple_push',
         }}
       >
         <Stack.Screen name="index" redirect={isUserAuthenticated} />
-        <Stack.Screen name="new" />
         <Stack.Screen name="memories" />
+        <Stack.Screen name="new" />
       </Stack>
     </ImageBackground>
   )
